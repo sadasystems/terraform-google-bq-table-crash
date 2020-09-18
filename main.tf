d6d194d1eafc37ddc2e14b0139b3d3ffb912558e
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2020-09-17 17:18:13 -0700

    Sample code for Terraform crash

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "google" {
+	project = "sada-slava-maslennikov"
+}
+
+resource "google_bigquery_dataset" "default" {
+  dataset_id = "something"
+}
+
+resource "google_bigquery_table" "default" {
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = "something"
+
+  schema = <<EOF
+[]
+EOF
+}
